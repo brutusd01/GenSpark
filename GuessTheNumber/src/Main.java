a471f50071053a7549c12e70b5f9afb32b51307c
DeRose Brutus <derose_brutus@hotmail.com>
2022-04-07 11:41:29 -0500

    Create functional guessing game

--- GuessTheNumber/src/Main.java
+++ GuessTheNumber/src/Main.java
@@ -1,20 +1,96 @@
+import java.util.Scanner;
+
+class Dialogue{ //Class to house the strings for dialogue.
+    //Dialogue lines!
+    static String intro = "Hello! Welcome to my game. First of all, what is your name?";
+    static String enter = "%nInput your first name below:";
+    static String enter2 = "%nGuess a whole number.";
+    static String noName = "%nToo shy huh? That's fine.";
+    static String line1 = "%nWell.. %s I have a number in mind." +
+            "%nIt is between 1 and 20." +
+            "%nTell me.. Can you guess it?";
+    static String high = "%nNope, lower.";
+    static String low = "%nNope, higher.";
+    static String win = "%nGood job, %s! It took you %s tries but you got it!";
+    static String firstTry = "%nReally? First try? You must be a wizard. You win.";
+    static String cont = "%nWanna go again, %s?(Y/N)";
+    static String newNum = "%nAwesome! I have a new number in mind.. Try to guess it!";
+    static String end = "%nThat's fine, hope you have a good one!";
+}
+
 public class Main {
 
+    public static int privateNum = (int)(Math.random() * 20) + 1;
+    static int guesses = 0;
+    static int userGuess;
+    public static String playerName = "";
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
-        //Game parameters
-        int privateNum = (int)(Math.random() * 20) + 1;
-        int guesses = 0;
-        String playerName = "";
+        System.out.println(Dialogue.intro);
+        System.out.println(String.format(Dialogue.enter));
+        playerName = scan.nextLine();
+        if (playerName == null || playerName.isEmpty()) {
+            playerName = "NoName";
+            System.out.println(String.format(Dialogue.noName));
+        }
+        System.out.println(String.format(Dialogue.line1, playerName));
+        //Start the guessing game.
 
-        //Dialogue lines!
-        String intro = "Hello! Welcome to my game. First of all, what is your name?";
-        String line1 = String.format("Well.. %s I have a number in mind." +
-                "%nIt is between 1 and 20." +
-                "%nTell me.. Can you guess it?", playerName);
+        GuessTheNumber();
 
-        //Print the lines.
-        System.out.println(intro);
-        System.out.println(line1);
     }
+
+    private static void GuessTheNumber() {
+        do
+        {
+            userGuess = scan.nextInt();
+            guesses++;
+            AttemptNumber(userGuess);
+        }
+        while(userGuess != privateNum);
+        System.out.println(String.format(Dialogue.cont, playerName));
+        switch (scan.next()) {
+            case "Y":
+                NewGame();
+                break;
+            case "N":
+                System.out.println(String.format(Dialogue.end));
+                return;
+        }
+    }
+
+    private static void NewGame() {
+        System.out.println(String.format(Dialogue.newNum));
+        privateNum = (int)(Math.random() * 20) + 1;
+        guesses = 0;
+        GuessTheNumber();
+    }
+
+    static void AttemptNumber(int guess) {
+            if(guess == privateNum)
+            {
+                if(guesses == 1)
+                {
+                    System.out.println(String.format(Dialogue.firstTry));
+                    return;
+                }
+                System.out.println(String.format(Dialogue.win, playerName, guesses));
+                return;
+            }
+            if(guess > privateNum)
+            {
+                System.out.println(String.format(Dialogue.high));
+                return;
+            }
+            if(guess < privateNum)
+            {
+                System.out.println(String.format(Dialogue.low));
+
+            }
+    }
+
 }
+
+
+
