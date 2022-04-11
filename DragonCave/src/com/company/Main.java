149577ed31bb16614c0145d6729eee7bead659fa
DeRose Brutus <derose_brutus@hotmail.com>
2022-04-11 12:40:08 -0500

    Catch exceptions to DragonCave

--- DragonCave/src/com/company/Main.java
+++ DragonCave/src/com/company/Main.java
@@ -2,51 +2,60 @@ package com.company;
 
 import java.util.Scanner;
 
+class Dialogue{
+    static String line1 = "%n" +
+            "You are in a land full of dragons.%nIn front of you, you see two caves. " +
+            "%nIn one cave, the dragon is friendly and will share his treasure with you." +
+            "%nThe other dragon is greedy and hungry and will eat you on sight." +
+            "%nWhich cave will you go into? (1 or 2)";
+
+    static String fail = "%n" +
+            "You approach the cave.." +
+            "%nIt is dark and spooky.. Like.. Very dark. You can't really see." +
+            "%nA large dragon jumps out in front of you! He opens his jaw and.." +
+            "%nYou got swallowed in one bite.";
+
+    static String win = "%n" +
+            "You approach the cave.." +
+            "%nYou find a big dragon on a pile of gold." +
+            "%nThe dragon gives you a chest full of gold. Good job!";
+}
+
 public class Main {
-    static int input;
+    static String input = "";
     public static void main(String[] args) {
 	// write your code here
-        String line1 = "%n" +
-                "You are in a land full of dragons.%nIn front of you, you see two caves. " +
-                "%nIn one cave, the dragon is friendly and will share his treasure with you." +
-                "%nThe other dragon is greedy and hungry and will eat you on sight." +
-                "%nWhich cave will you go into? (1 or 2)";
-
-        String fail = "%n" +
-                "You approach the cave.." +
-                "%nIt is dark and spooky.. Like.. Very dark. You can't really see." +
-                "%nA large dragon jumps out in front of you! He opens his jaw and.." +
-                "%nYou got swallowed in one bite.";
-
-        String win = "%n" +
-                "You approach the cave.." +
-                "%nYou find a big dragon on a pile of gold." +
-                "%nThe dragon gives you a chest full of gold. Good job!";
-
-
-
-        System.out.println(String.format(line1));
+        System.out.println(String.format(Dialogue.line1));
         Scanner console = new Scanner(System.in);
         String dialogue = "";
-
-           getInput: while(console.hasNextInt())
+        Boolean end = false;
+        int choice;
+           while(!end)
            {
-            input = console.nextInt();
-            switch (input) {
-                   case 1:
-                       dialogue = fail;
-                       break getInput;
-                   case 2:
-                       dialogue = win;
-                       break getInput;
-                   default:
-                       //dialogue = invalid;
-                       System.out.println("Invalid.");
-               }
-               System.out.println("\033[1m" + input+"\033[0m");
-           }
+               input = console.nextLine();
 
+               try {
+                  choice = Integer.parseInt(input);
+                  System.out.println("\033[1m" + input+"\033[0m");
+
+                   switch (choice) {
+                       case 1:
+                           dialogue = Dialogue.fail;
+                           end = true;
+                           break;
+                       case 2:
+                           dialogue = Dialogue.win;
+                           end = true;
+                           break;
+                   }
+               }
+               catch(Exception e)
+               {
+                   System.out.println("Enter 1 or 2!");
+               }
+           }
         System.out.println(String.format(dialogue));
+        console.next();
 
     }
 
