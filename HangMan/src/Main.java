d984f56ec8ae3d8a000c0c9b045e217092349fd5
DeRose Brutus <derose_brutus@hotmail.com>
2022-04-23 16:20:12 -0500

    Create loopable hangman game

--- HangMan/src/Main.java
+++ HangMan/src/Main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
-    public static String wordToGuess = "cat";
+    public static String[] words = {"cat", "devious", "genspark", "java", "gosling", "reptilia", "megadeth" };
+    public static String wordToGuess = "";
     public static String hiddenAnswer = "";
-   // public static char[] answerArray;
     public static ArrayList<String> missedWords = new ArrayList<>();
     public static int guessesLeft = 6;
     public static StringBuilder sb;
@@ -11,45 +12,55 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        for (int i = 0; i < wordToGuess.length(); i++) {
-            hiddenAnswer += "_";
-        }
+        HangMan();
+        GameOver(false);
+    }
 
-        sb = new StringBuilder(hiddenAnswer);
-
-        System.out.println("H A N G   M A N");
-        System.out.println(String.format("Guess the word:" +
-                "%n%s",hiddenAnswer));
+    private static void HangMan() {
+        ResetGame();
         do
         {
+            if(sb.toString().contains(wordToGuess)) {
+            GameOver(true);
+            return;
+        }
             try
             {
                 var input = scan.next();
                 GuessWord(input);
                 System.out.println(sb);
-
             } catch (Exception e)
             {
-                System.out.println("Caught exception, type in words only.");
+                System.out.println("Caught exception: Invalid input type.");
             }
 
         }while(guessesLeft > 0);
-        if(guessesLeft == 0)
+    }
+
+    public static void ResetGame() {
+        int random = (int)(Math.random() * words.length);
+        wordToGuess = words[random];
+        hiddenAnswer = "";
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            hiddenAnswer += "_";
+        }
+        sb = new StringBuilder(hiddenAnswer);
+        System.out.println("H A N G   M A N");
+        System.out.println(String.format("Guess the word:" +
+                "%n%s",hiddenAnswer));
+    }
+
+    public static void GuessWord(String guess)
+    {
+        if(missedWords.contains(guess) || sb.toString().contains(guess))
         {
-            GameOver();
+            System.out.println(String.format("%nYou already guessed that letter. Pick a new one."));
+            return;
         }
 
-    }
-
-    private static void GameOver() {
-        System.out.println("Game Over! Want to try again?");
-    }
-
-    private static void GuessWord(String guess)
-    {
         char correct;
         if(guess.length() != 1) {
-            System.out.println("Try typing in just 1 letter.");
+            System.out.println(String.format("%nTry typing in just 1 letter."));
             return;
         }
 
@@ -59,12 +70,32 @@ public class Main {
             int index = wordToGuess.indexOf(guess);
             correct = guess.charAt(0);
             sb.setCharAt(index,correct);
+
             return;
         }
         else //if you got a wrong guess
             guessesLeft--;
             if(!missedWords.contains(guess))missedWords.add(guess);
         System.out.println(String.format("%n%s guesses left." +
-                "%nWordsGuessed: %s",guessesLeft,missedWords));
+                "%nWordsGuessed: %s",guessesLeft,missedWords.toString()));
+    }
+
+    public static void GameOver(boolean won) {
+        if(!won)System.out.println(String.format("%nThe word was %s. You lost.",wordToGuess));
+        else if(won)System.out.println(String.format("%nCorrect! The word was %s.",wordToGuess));
+        System.out.println(String.format("%nWant to try again? (Y/N)"));
+        boolean good = false;
+        while(!good) {
+            switch (scan.next().toString()) {
+                case "Y":
+                case "y":
+                    good = true;
+                    HangMan();
+                    break;
+                case "N":
+                case "n":
+                    System.exit(0);
+            }
+        }
     }
 }
