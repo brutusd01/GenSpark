15104dc668e5cff32d9942410cd36672aa8d67d9
DeRose Brutus <derose_brutus@hotmail.com>
2022-05-26 13:18:24 -0500

    Setup Week 8 assignment

---
+++ Week8SpringFramework/src/main/java/org/genspark/App.java
@@ -0,0 +1,12 @@
+package org.genspark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+
+    public static void main(String[] args) {
+    }
+}
---
+++ Week8SpringFramework/src/main/java/org/genspark/Student.java
@@ -0,0 +1,11 @@
+package org.genspark;
+
+import java.util.List;
+
+public class Student
+{
+    private int id;
+    private String name;
+    private List<Phone> ph;
+    private Address add;
+}
